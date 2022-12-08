0d13aa0ff960a42eabe1608d01a0ade886b6f46f
Claylton Roque <claylton202@gmail.com>
2022-12-07 22:38:01 -0300

    correction build

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../views/Home.vue";
 import Favorites from "../views/Favorites.vue";
+import Home from "../views/Home.vue";
 
 const routes = [
   {
